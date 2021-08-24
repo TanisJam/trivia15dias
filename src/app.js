1194a7dda2731611de6eda243039d1cca603d967
Mauricio Romero <mauricionromero@hotmail.com>
2021-08-24 18:02:58 -0300

    añadido footbar

--- src/app.js
+++ src/app.js
@@ -6,8 +6,12 @@ import "bootstrap";
 const parser = new DOMParser();
 const nav = require("html-loader!./partials/nav.html").default;
 const navHTML = parser.parseFromString(nav, "text/html");
-
 document.querySelector("body").prepend(navHTML.body.firstChild);
+
+const footer = require("html-loader!./partials/footer.html").default;
+const footerHTML = parser.parseFromString(footer, "text/html");
+document.querySelector("body").append(footerHTML.body.firstChild);
+
 
 
 //Styles
